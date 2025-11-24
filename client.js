c338640a7c87b0d3154df286a16e58ef687a2498
Elliot Lyhne <Hunterlyhne@gmail.com>
2025-11-24 19:05:09 +0100

    Query logic and fixed Malmø

--- client.js
+++ client.js
@@ -2,33 +2,51 @@
 const testField = document.querySelector("#testField");
 console.log(EKdataset[0]);
 
-// clean dataset
-for (const element of EKdataset) {
-    let kvotient = element.KVOTIENT
-    if (kvotient){
-        if (typeof(kvotient)==="string") {
-            kvotient = kvotient.replace(',', '.')
-            kvotient = parseFloat(kvotient)
-            element.KVOTIENT = kvotient
+
+function cleanDataset() {
+    // clean dataset
+    for (const element of EKdataset) {
+        // clean kvotient string to float number. i.e "8,6" to 8.6
+        let kvotient = element.KVOTIENT
+        if (kvotient){
+            if (typeof(kvotient)==="string") {
+                kvotient = kvotient.replace(',', '.')
+                kvotient = parseFloat(kvotient)
+                element.KVOTIENT = kvotient
+            }
         }
-        
+        // clean Malmø
+
     }
 }
 
 
-const query = {
-    "Køn": "Mand"
-};
 
 
-const filteredData = EKdataset.filter(sift.default(query)); 
+function getQuotaGender(gender){
+    const pipeline = [
+        {
+            $match: { "Køn": gender }
+        },
+        {
+            $group: {
+                _id: null,
+                AVGKVOTIENT: { $avg: "$KVOTIENT" }
+            }
+        }
+    ]
 
-for (const element of filteredData) {
-    let newLi = document.createElement("li");
-    newLi.textContent = element.KVOTIENT;
-    testField.appendChild(newLi);
+    const queryResult = new mingo.Aggregator(pipeline).run(EKdataset);    
+    
+    const result = queryResult[0].AVGKVOTIENT.toFixed(2)
+    // .toFixed(2) limit and rounds to 2 decimals
+    
+    console.log(result);
+    // logs("Mand"): 6.11
+    // logs("Kvinde"): 6.83
 }
 
+getQuotaGender("Kvinde")
 
 
 
